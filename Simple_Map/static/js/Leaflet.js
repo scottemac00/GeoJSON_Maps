8e0031558c2583b85d3e7069171af60467754ccd
scottemac00 <scott.e.mccraney@gmail.com>
2023-01-09 18:02:13 -0500

    Thru 14.2.4

---
+++ Simple_Map/static/js/Leaflet.js
@@ -0,0 +1,4 @@
+// Make sure you put this AFTER Leaflet's CSS
+<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+    crossorigin=""></script>
